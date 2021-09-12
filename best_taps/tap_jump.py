cb5fc896e953dc339356333ed8cdf75bba1d572a
asgarieh <65185755+SamanAsgarieh@users.noreply.github.com>
2021-09-12 14:55:39 -0600

    which taps to water the plants

--- best_taps/tap_jump.py
+++ best_taps/tap_jump.py
@@ -1,15 +1,5 @@
 class Solution:
     def minTaps(self, n, ranges) -> int:
-        # start = end = taps = 0
-        # while end<n:
-        #     for i in range (len(ranges)):
-        #         if i-ranges[i]<=start and i+ranges[i]>end:
-        #             end=i+ranges[i]
-        #         if start == end:
-        #             return -1
-        #         taps +=1
-        #         start = end
-        # return taps
         start, end = 0, 0
         taps = 0
         
